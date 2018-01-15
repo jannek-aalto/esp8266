0ca4f4358c3022ef7ff2eb03ad629661bef6f576
Janne Korkkula <janne.korkkula@aalto.fi>
2018-01-15 12:34:59 +0200

    Add files via upload
    //   jk 2018-01-15
    //
    //   NTP server configuration changed (now defined here)
    //
    //   European Union daylight saving time (DST) support added,
    //   needed to add a bit of code to NTPClient for this too, a
    //   function to get the month as a number: int NTPClient::getMon()
    //   *TODO: move the DST code to NTPClient.cpp*
    //
    //   Blink code changed, slowly blinks the ":" 3 times before the 
    //   minute changes, switching it off for seconds 55, 57 and 59.
    //
    //   OTA requests handled at end of loop (less latency, possibly)
    //

--- d1-ntp-clock/d1-ntp-clock.ino
+++ d1-ntp-clock/d1-ntp-clock.ino
@@ -7,6 +7,21 @@
 //   Steve
 //   --
 //
+//
+//   jk 2018-01-15
+//
+//   NTP server configuration changed (now defined here)
+//
+//   European Union daylight saving time (DST) support added,
+//   needed to add a bit of code to NTPClient for this too, a
+//   function to get the month as a number: int NTPClient::getMon()
+//   *TODO: move the DST code to NTPClient.cpp*
+//
+//   Blink code changed, slowly blinks the ":" 3 times before the 
+//   minute changes, switching it off for seconds 55, 57 and 59.
+//
+//   OTA requests handled at end of loop (less latency, possibly)
+//
 
 
 //
@@ -19,6 +34,9 @@
 // For dealing with NTP & the clock.
 //
 #include "NTPClient.h"
+#define NTP_UPDATE_FREQ 3600 // seconds
+#define NTP_SERVER "europe.pool.ntp.org"
+//#define NTP_SERVER "192.168.1.1"
 
 //
 // The display-interface
@@ -45,20 +63,25 @@
 //   Access-Point name, in config-mode
 //   OTA name.
 //
-#define PROJECT_NAME "NTP-CLOCK"
+#define PROJECT_NAME "NTP-clock"
 
 
 //
-// The timezone - comment out to stay at GMT.
+// The timezone
 //
-#define TIME_ZONE (+3)
+// To honour European Union Daylight Saving Time (DST) rules:
+// define TIME_ZONE 0, define TZ_OFFSET as hours and define EU_DST
+//
+#define TIME_ZONE 0
+#define TZ_OFFSET 2
+#define EU_DST
 
 
 //
 // NTP client, and UDP socket it uses.
 //
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+NTPClient timeClient(ntpUDP, NTP_SERVER, TIME_ZONE * 3600, NTP_UPDATE_FREQ * 1000);
 
 
 //
@@ -74,7 +97,8 @@ TM1637 tm1637(CLK, DIO);
 //
 void on_before_ntp()
 {
-    DEBUG_LOG("Updating date & time");
+    DEBUG_LOG("Updating date & time\n");
+    tm1637.point(POINT_OFF);
 }
 
 //
@@ -83,6 +107,7 @@ void on_before_ntp()
 void on_after_ntp()
 {
     DEBUG_LOG("Updated NTP client\n");
+    tm1637.point(POINT_ON);
 }
 
 //
@@ -97,7 +122,7 @@ void setup()
     tm1637.init();
 
     // We want to see ":" between the digits.
-    tm1637.point(true);
+    tm1637.point(POINT_ON);
 
     //
     // Set the intensity - valid choices include:
@@ -125,12 +150,6 @@ void setup()
     //
     timeClient.on_before_update(on_before_ntp);
     timeClient.on_after_update(on_after_ntp);
-
-    //
-    // Setup the timezone & update-interval.
-    //
-    timeClient.setTimeOffset(TIME_ZONE * (60 * 60));
-    timeClient.setUpdateInterval(300 * 1000);
 
 
     //
@@ -178,52 +197,93 @@ void setup()
     // Ensure the OTA process is running & listening.
     //
     ArduinoOTA.begin();
-
 }
 
 
 
 //
-// This function is called continously, and is responsible
-// for flashing the ":", and otherwise updating the display.
+// This function is called continously and is responsible
+// for flashing the ":" and otherwise updating the display.
 //
 // We rely on the background NTP-updates to actually make sure
 // that that works.
 //
+// Evaluates EU DST rules.
+//
 void loop()
 {
     static char buf[10] = { '\0' };
-    static char prev[10] = { '\0' };
-    static long last_read = 0;
-    static bool flash = true;
-
+    static char prev_buf[10] = { '\0' };
+    static int  prev_sec = 0;
+    
     //
     // Resync the clock?
     //
     timeClient.update();
 
     //
-    // Handle any pending over the air updates.
-    //
-    ArduinoOTA.handle();
-
-    //
     // Get the current hour/min
     //
+    int cur_wday = timeClient.getDay(); // 0-6,  0 == Sun
+    int cur_mon  = timeClient.getMon(); // 1-12
+    int cur_day  = timeClient.getDayOfMonth(); // 1-31
+    
     int cur_hour = timeClient.getHours();
     int cur_min  = timeClient.getMinutes();
+    int cur_sec  = timeClient.getSeconds();
+
+    int disp_hour = cur_hour + TZ_OFFSET; // 0-24h overflow handled later
+
+#ifdef EU_DST
+    //
+    // EU DST
+    //
+    if ((cur_mon < 3) || (cur_mon > 10)) {
+        // Jan-Feb, Nov-Dec: Normal time
+        // exit if
+    } else if ((cur_mon > 3) && (cur_mon < 10)) {
+        // Apr-Sep: DST
+        disp_hour++;
+    } else if (cur_mon == 3) {
+        // March
+        if ((cur_day > 24) && ((!cur_wday && cur_hour) || (cur_wday && (cur_day - cur_wday > 24)))) {
+            // Last Sunday of March, 01:00 UTC and after: DST
+            disp_hour++;
+        }
+    } else if (cur_day < 25) {
+        // October, all before 25th: DST
+        disp_hour++;
+    } else if (!((!cur_wday && cur_hour) || (cur_wday && (cur_day - cur_wday > 24)))) {
+        // October 25th and after, but before last Sunday 01:00 UTC: DST
+        disp_hour++;
+    } // else normal time.
+#endif
+
+    // Fix hour overflow
+    if (disp_hour > 23) disp_hour -= 24;
+    if (disp_hour <  0) disp_hour += 24;
 
     //
-    // Format them in a useful way.
+    // Format in a useful way.
     //
-    sprintf(buf, "%02d%02d", cur_hour, cur_min);
+    sprintf(buf, "%02d%02d", disp_hour, cur_min);
 
     //
-    // If the current "hourmin" is different to
-    // that we displayed last loop ..
+    // If the second has changed and the 
+    // minute is about to change soon...
     //
-    if (strcmp(buf, prev) != 0)
-    {
+    if (prev_sec != cur_sec) {
+        if (cur_sec > 54 || !cur_sec) {
+            tm1637.point(!(cur_sec % 2));     // Boolean option, conveniently
+            memset(prev_buf, '\0', sizeof(prev_buf)); // Force display update
+        }
+        prev_sec = cur_sec;
+    }
+    
+    //
+    // If the current "hourmin" has changed...
+    //
+    if (strcmp(buf, prev_buf)) {
         // Update the display
         tm1637.display(0, buf[0] - '0');
         tm1637.display(1, buf[1] - '0');
@@ -231,39 +291,11 @@ void loop()
         tm1637.display(3, buf[3] - '0');
 
         // And cache it
-        strcpy(prev , buf);
-
+        strcpy(prev_buf, buf);
     }
 
-
     //
-    // The preceeding piece of code would
-    // have ensured the display only updated
-    // when the hour/min changed.
+    // Handle any pending over the air updates.
     //
-    // However note that we nuke the cached
-    // value every half-second - solely so we can
-    // blink the ":".
-    //
-    //  Sigh
-
-    long now = millis();
-
-    if ((last_read == 0) ||
-            (abs(now - last_read) > 500))
-    {
-        // Invert the "show :" flag
-        flash = !flash;
-
-        // Apply it.
-        tm1637.point(flash);
-
-        //
-        // Note that the ":" won't redraw unless/until you update.
-        // So we'll force that to happen by removing the cached
-        // value here.
-        //
-        memset(prev, '\0', sizeof(prev));
-        last_read = now;
-    }
+    ArduinoOTA.handle();
 }
